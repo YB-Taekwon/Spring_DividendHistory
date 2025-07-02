9c76c3f8e4b7a7c0fa808b088e3960b71699dc1b
YB-Taekwon <loundo001@gmail.com>
2025-07-02 17:10:59 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/ian/finance/DividendApplication.java
@@ -0,0 +1,11 @@
+package com.ian.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DividendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ian/finance/DividendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ian.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
